46c4df513f39347dcfbd5eaac29611e075313744
syp.1209 <1yu2na0mi9@gmail.com>
2024-12-26 21:01:50 +0900

    お気に入り機能１

--- samuraitravel/src/main/java/com/example/samuraitravel/controller/FavoriteController.java
+++ samuraitravel/src/main/java/com/example/samuraitravel/controller/FavoriteController.java
@@ -1,8 +1,16 @@
 package com.example.samuraitravel.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.samuraitravel.entity.House;
+import com.example.samuraitravel.entity.User;
 import com.example.samuraitravel.repository.HouseRepository;
+import com.example.samuraitravel.security.UserDetailsImpl;
+
+import ch.qos.logback.core.model.Model;
 
 @Controller
 public class FavoriteController {
@@ -17,19 +25,58 @@ public class FavoriteController {
 	}
 	
     //お気に入り一覧ページ
-    //@GetMapping("/houses/favoriteList")
-    //public String showFavoriteList(
-    //		@AuthenticationPrincipal UserDetailsImpl userDetailsImpl,
-    //		Model model) {
+    @GetMapping("/favoriteList")
+    public String showFavoriteList(
+    		@AuthenticationPrincipal UserDetailsImpl userDetailsImpl,
+    		Model model) {
+				
     	
     	//userDetailsImplを条件に
     	//紐づくFavoritesテーブルの一覧を取得する。
-       // List<FavoriteListForm> favorites = favoriteService.findFavoritesByUserId(userDetailsImpl.getUser().getId());
+    	//List<FavoriteListForm> favorites = FavoriteService.findFavoritesByUserId(userDetailsImpl.getUser().getId());
         
         //House house = houseRepository.getReferenceById(houseId);
         //User user = userDetailsImpl.getUser();
-        
-      
+    	
+    	return null ;
+    }
+    
+    
+    //お気に入りリスト追加
+    @GetMapping("/favorite/input")
+    public String inputFavorites(
+    		@AuthenticationPrincipal UserDetailsImpl userDetailsImpl,
+    		@RequestParam Integer houseId,
+    		Model model) {
+    	
+		House house = houseRepository.getReferenceById(houseId);
+		User user = userDetailsImpl.getUser();
+		//model.addAttribute("house", house);
+		//model.addAttribute("user", user);
+		
+		return "/favoriteList";
+    }
+    
+    
+    //お気に入りリスト削除
+    @GetMapping("/favorite/delete")
+    public String deleteFavorites(
+    		@AuthenticationPrincipal UserDetailsImpl userDetailsImpl,
+    		@RequestParam Integer houseId,
+    		Model model) {
+    	
+    	
+		House house = houseRepository.getReferenceById(houseId);
+		User user = userDetailsImpl.getUser();
+		
+		//model.addAttribute("house", house);
+		//model.addAttribute("user", user);
+		
+		//FavoritesRepository.deleteByHouseIdandUserId(houseId,userDetailsImpl.getUser().getId());
+		return "/favoriteList";
+		
+    }
+    
     }
     
 //}
--- samuraitravel/src/main/java/com/example/samuraitravel/service/FavoriteService.java
+++ samuraitravel/src/main/java/com/example/samuraitravel/service/FavoriteService.java
@@ -1,9 +1,36 @@
 package com.example.samuraitravel.service;
 
-public class FavoriteService {
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.samuraitravel.entity.Favorites;
+import com.example.samuraitravel.entity.House;
+import com.example.samuraitravel.entity.User;
+import com.example.samuraitravel.repository.FavoritesRepository;
+import com.example.samuraitravel.repository.HouseRepository;
+import com.example.samuraitravel.repository.UserRepository;
+
+public class FavoriteService {	
+	private final HouseRepository houseRepository;
+	private final UserRepository userRepository;
+	private final FavoritesRepository favoritesRepository;
+	
+	public FavoriteService(
+		FavoritesRepository favoritesRepository,
+		HouseRepository houseRepository,
+		UserRepository userRepository
+		) {
+
+			this.favoritesRepository = favoritesRepository;
+			this.houseRepository = houseRepository;
+			this.userRepository = userRepository;
+			}
+	
+
+	
+	
 	//お気に入り一覧ページに表示するデータを取得する
     //public List<FavoriteListForm> findFavoritesByUserId(int UserId) {
-     //   return getFavoriteHouseList(FavoritesRepository.findFavoritesByUserId(UserId));
+    //    return getFavoriteHouseList(FavoritesRepository.findByUserId(UserId));
     //}
     
     
@@ -16,4 +43,17 @@ public class FavoriteService {
     	
     //	return getFavoriteHouseList;
     //}
+    
+	//Favoriteテーブルに追加する
+	@Transactional
+	public void input(int houseId, int userId) {
+	    Favorites favorites = new Favorites();
+        House house = houseRepository.getReferenceById(houseId);
+        User user = userRepository.getReferenceById(userId);
+        
+        favorites.setHouse(house);
+        favorites.setUser(user);
+
+        favoritesRepository.save(favorites);
+	}
 }
